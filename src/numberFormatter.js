b79026e5b56e61cf902a39134257066e139804bd
Eduardo Milpax <eduardomilpas@gmail.com>
2019-05-22 21:50:56 -0400

    fix, number formatting when module contains less than 3 digits, completes with zero on the left

--- src/numberFormatter.js
+++ src/numberFormatter.js
@@ -1,19 +1,25 @@
-
-
-export function format(n) {
+    export function format(n) {
     n = formatNumber(n)
     return n.startsWith(",") ? n.substring(1, n.length) : n
 }
 
 function formatNumber(n) {
-    if(n / 1000 > 0) {
+    if(Math.floor((n / 1000)) > 0) {
         let div = Math.floor(n / 1000)
         let mod = Math.floor(n % 1000)
         mod = mod === 0 ? '000' : mod
 
-        return formatNumber(div) + ',' + mod
+        return formatNumber(div) + ',' + getZeros('' + mod)
     }
     else {
         return n > 0 ? n : ''
     }
+}
+
+function getZeros(n) {
+    let formattedN = n
+    for(let i = 0 ; i < 3 - n.length; i++) {
+        formattedN = '0' + formattedN
+    }
+    return formattedN
 }
